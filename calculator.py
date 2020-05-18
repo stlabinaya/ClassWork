caea4a07761f0a2c75cb311b9a53b7f90b384b42
Abinaya Lakshmanan <al325@duke.edu>
2020-05-18 10:31:39 -0500

    Made each calculator ability its own function

--- calculator.py
+++ calculator.py
@@ -1,30 +1,50 @@
 # calculator.py
 
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-a = int(a)
-b = int(b)
-print("The numbers you entered are {} and {}".format(a, b))
+def calc_input():
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+    a = int(a)
+    b = int(b)
+    print("The numbers you entered are {} and {}".format(a, b))
+    return a, b
 
-c = input("Enter a command: ")
-
-if c == 'a':
+def add(a, b):
     print("Add")
     answer = a + b
     print("{} + {} = {}".format(a, b, answer))
-elif c == 's':
+    return
+
+def subtract(a, b):
     print("Subtract")
     answer = a - b
     print("{} - {} = {}".format(a, b, answer))
-elif c == 'm':
+    return
+
+def multiply(a, b):
     print("Multiply")
-    answer = a*b
+    answer = a * b
     print("{} * {} = {}".format(a, b, answer))
-elif c == 'd':
+    return
+
+def divide(a, b):
     print("Divide")
     answer = a / b
     print("{} / {} = {}".format(a, b, answer))
-else:
-    print("{} is not a valid command".format(c))
+    return
 
+def math_command(a, b):
+    c = input("Enter a command: ")
+    if c == 'a':
+        add(a, b)
+    elif c == 's':
+        subtract(a, b)
+    elif c == 'm':
+        multiply(a, b)
+    elif c == 'd':
+        divide(a, b)
+    else:
+        print("{} is not a valid command".format(c))
+
+x, y = calc_input()
+math_command(x, y)
 print("Finished")
